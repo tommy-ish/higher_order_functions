295afcc6ce9f4e2a5e9ecd86382546ca4b06e8f6
石井智大 <ishii@MacBook-Air.local>
2021-02-06 10:58:53 +0900

    first commit

---
+++ lib/higher_order_functions.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "higher_order_functions/version"
+
+module HigherOrderFunctions
+  class Error < StandardError; end
+  # Your code goes here...
+end
